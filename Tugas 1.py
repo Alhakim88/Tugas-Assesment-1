0d05a9d417b7e04a1e576c27b16bd502ca39e353
Al Hakim 1988 <116581267+Alhakim88@users.noreply.github.com>
2026-01-11 23:21:41 +0700

    Add files via upload

---
+++ Tugas 1.py
@@ -0,0 +1,9 @@
+print("Hello there! I'm a simple chatbot. Let's get to know each other.")
+
+name = input("What is your name: ")
+age = input("How old are you: ")
+food = input("What is your favorite food: ")
+
+print("\nAwesome! So, your name is", name + ",",
+      "you are", age, "years old, and",
+      "you love to eat", food + ". Nice to meet you!")
