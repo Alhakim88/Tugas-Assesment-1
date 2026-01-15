1a5477d9e021cdf49be176a94512401089ae5280
Al Hakim 1988 <116581267+Alhakim88@users.noreply.github.com>
2026-01-15 21:16:11 +0700

    Add files via upload

---
+++ Tugas 1b.py
@@ -0,0 +1,18 @@
+# Status awal player
+player_hp = 100
+
+print("Player HP awal:", player_hp)
+
+# Player minum potion
+healing_potion = 25
+player_hp += healing_potion
+print("Player minum Health Potion +25 HP")
+print("HP sekarang:", player_hp)
+
+# Player diserang monster
+monster_attack = 40
+player_hp -= monster_attack
+print("Monster menyerang! -40 HP")
+
+# HP akhir
+print("HP akhir Player:", player_hp)
